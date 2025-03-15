f6d98808a75f7754750568287e20b59cb71e8573
Spidey00007 <asamit1547k@gmail.com>
2025-03-16 02:06:34 +0530

    Fixed deployment failure caused by filename mismatch

--- src/components/providers/StreamClientProvider.tsx
+++ src/components/providers/StreamClientProvider.tsx
@@ -3,7 +3,7 @@
 import { ReactNode, useEffect, useState } from "react";
 import { StreamVideoClient, StreamVideo } from "@stream-io/video-react-sdk";
 import { useUser } from "@clerk/nextjs";
-import LoaderUI from "../LoaderUi";
+import LoaderUI from "../LoaderUI";
 import { streamTokenProvider } from "@/actions/stream.actions";
 
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
